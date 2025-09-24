1e9ffc0e2d426a459745f2957027cc6368fe65b5
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-09-24 11:04:49 -0400

    pull out the clumper utils to dr_ingest

--- src/dr_ingest/__init__.py
+++ src/dr_ingest/__init__.py
@@ -3,6 +3,13 @@
 from .normalization import key_variants, normalize_key, split_by_known_prefix
 from .serialization import compare_sizes, dump_runs_and_history, ensure_parquet
 from .qa import ensure_extracted, list_tarballs
+from .qa.schemas import ModelAnswerOutput, QuestionOutputData, TaskOutputData
+from .qa.transform import (
+    build_file_metadata,
+    extract_question_payloads,
+    preview_agg_metrics,
+    model_output_keys,
+)
 from .wandb.summary import normalize_oe_summary
 
 __all__ = [
@@ -15,4 +22,11 @@ __all__ = [
     "normalize_oe_summary",
     "list_tarballs",
     "ensure_extracted",
+    "ModelAnswerOutput",
+    "QuestionOutputData",
+    "TaskOutputData",
+    "build_file_metadata",
+    "extract_question_payloads",
+    "preview_agg_metrics",
+    "model_output_keys",
 ]
