de6d56f0b5e1953f57da5a3ff0d7babf26574b77
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-11-10 15:37:20 -0500

    Cleanup scaling laws parsing

--- src/dr_ingest/pipelines/__init__.py
+++ src/dr_ingest/pipelines/__init__.py
@@ -6,10 +6,12 @@ from .dd_results import (
     parse_size_string,
     parse_train_df,
 )
+from .dd_scaling_laws import parse_scaling_law_dir
 
 __all__ = [
     "add_task_group",
     "parse_metrics_col",
+    "parse_scaling_law_dir",
     "parse_size_string",
     "parse_train_df",
 ]
